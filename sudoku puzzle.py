0a2d5340d0ed29881a0e36ef3e6d373efd452a7e
saniyakulkarni16 <saniyakulkrni16@gmail.com>
2025-06-28 19:13:46 +0530

    Add files via upload

---
+++ sudoku puzzle.py
@@ -0,0 +1,72 @@
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+def print_board(board):
+    for i in range(9):
+        if i % 3 == 0 and i != 0:
+            print("-" * 21)
+        for j in range(9):
+            if j % 3 == 0 and j != 0:
+                print("|", end=" ")
+            print(board[i][j] if board[i][j] != 0 else ".", end=" ")
+        print()
+def find_empty(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i, j)  # row, col
+    return None
+def is_valid(board, num, pos):
+    row, col = pos
+
+    # Check row
+    if num in board[row]:
+        return False
+
+    # Check column
+    if num in [board[i][col] for i in range(9)]:
+        return False
+
+    # Check 3x3 grid
+    box_x = col // 3
+    box_y = row // 3
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num:
+                return False
+
+    return True
+def solve(board):
+    empty = find_empty(board)
+    if not empty:
+        return True  # Solved
+    row, col = empty
+
+    for num in range(1, 10):
+        if is_valid(board, num, (row, col)):
+            board[row][col] = num
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0  # Backtrack
+
+    return False
+print("Initial Sudoku Board:")
+print_board(board)
+
+if solve(board):
+    print("\nSolved Sudoku Board:")
+    print_board(board)
+else:
+    print("No solution exists")
